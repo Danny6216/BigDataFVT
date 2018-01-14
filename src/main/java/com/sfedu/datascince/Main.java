e9a373c885bd6c0075747ad4598097225fefa414
Danny6216 <dann6216@gmail.com>
2018-01-15 01:48:04 +0300

    работающая версия

--- src/main/java/com/sfedu/datascince/Main.java
+++ src/main/java/com/sfedu/datascince/Main.java
@@ -1,13 +1,13 @@
 package com.sfedu.datascince;
 
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -20,34 +20,35 @@ public class Main {
         try {
             String inPath = "/home/dann/Documents/bigData/";
             String outPath = "/home/dann/Documents/bigData/output/";
-            benchmarkProcessor(1, inPath, outPath);
+            benchmarkProcessor(2, inPath, outPath);
         }catch (Exception ex){
             System.out.println("Exception: " + ex);
         }
     }
     public static void benchmarkProcessor(int benchmark, String inPath, String outPath){
         try {
+            String filename = outPath + "bench_" + benchmark +"_out.xlsx" ;
+            SXSSFWorkbook workbook = new SXSSFWorkbook(100);
+            Sheet sheet = workbook.createSheet("Sheet");
             for (int k = 1; k <= 73; k++) {
-                String filename = outPath + "bench_" + benchmark +"_out" ;
-                XSSFWorkbook workbook = new XSSFWorkbook();
-                XSSFSheet sheet = workbook.createSheet("FirstSheet");
                 double[][] bench = bench2arr(inPath + "Benchmark_" + benchmark + "_sniff_" + k + ".txt");
                 List<Double>list = arrayToCorrelMatrix(bench);
                 System.out.println("result: Benchmark_" + benchmark + "_sniff_" + k);
 //                for(Double item : list){
 //                    System.out.print(String.format("%.2f",item) + "; ");
 //                }
+                Row row = sheet.createRow(k);
+                row.createCell(0).setCellValue("Benchmark " + benchmark + " sniff " + k);
                 for(int i = 0; i < list.size(); i++){
-                    XSSFRow row = sheet.createRow(k+2);
+
                     row.createCell(i+1).setCellValue(list.get(i));
                 }
-                System.out.println();
-
-                FileOutputStream fileOut = new FileOutputStream(filename);
-                workbook.write(fileOut);
-                fileOut.close();
-                System.out.println("Your excel file has been generated!" + filename);
             }
+            FileOutputStream fileOut = new FileOutputStream(filename);
+            workbook.write(fileOut);
+            fileOut.close();
+            workbook.dispose();
+            System.out.println("Your excel file has been generated!" + filename);
         }catch (Exception ex){
             System.out.println("Exception: " + ex);
         }
